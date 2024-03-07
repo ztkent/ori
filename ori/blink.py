6f980c8d5dd99ae56af9c95e59038106f1aa88d8
Zachary Kent <ztkent@gmail.com>
2024-03-07 18:45:20 -0500

    strobe

--- ori/blink.py
+++ ori/blink.py
@@ -1,3 +1,4 @@
+import random
 from time import sleep
 from blinkstick import blinkstick
 
@@ -13,6 +14,21 @@ def notify_alerts(alerts: dict):
         print(f"Found BlinkStick: {bstick.get_serial()}")
         for alert_type, _ in alerts.items():
             print(alert_type)
-            bstick.set_color(red=255,green=0,blue=0)
+            strobe_light()
+
+def strobe_light():
+    # Check if there are any BlinkStick devices connected
+    if len(blinkstick.find_all()) == 0:
+        print("No BlinkStick devices found.")
+        return
+
+    # Strobe the light
+    print("Strobing light.")
+    for bstick in blinkstick.find_all():
+        print(f"Found BlinkStick: {bstick.get_serial()}")
+        for _ in range(5):
+            color_hex = int(random.random()*0xFFFFFF)
+            bstick.set_color(hex=color_hex)
             sleep(1)
             bstick.turn_off()
+            sleep(0.5)
