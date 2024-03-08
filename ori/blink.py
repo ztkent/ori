f33801400ad5903338660adbd9f2a40a0903c89a
Zachary Kent <ztkent@gmail.com>
2024-03-08 14:47:30 -0500

    assign events colors

--- ori/blink.py
+++ ori/blink.py
@@ -2,30 +2,37 @@ import random
 from time import sleep
 from blinkstick import blinkstick
 
+NOTIFICATION_COLORS = {
+    "unread Gmail messages":          (255, 0, 0),     # Red
+    "today's Google Calendar events": (0, 255, 0),     # Green
+    "unread Outlook emails":          (0, 0, 255),     # Blue
+    "today's calendar events":        (255, 255, 0),   # Yellow
+    "unread Teams chat messages":     (255, 0, 255),   # Magenta
+    "unread Teams channel messages":  (0, 255, 255),   # Cyan
+}
+
 def notify_alerts(alerts: dict):
-    # Check if there are any BlinkStick devices connected
     if len(blinkstick.find_all()) == 0:
         print("No BlinkStick devices found.")
         return
-
+    
     # Notify the alerts
     print("Notifying alerts.")
     for bstick in blinkstick.find_all():
         print(f"Found BlinkStick: {bstick.get_serial()}")
         for alert_type, _ in alerts.items():
             print(alert_type)
-            bstick.set_color(red=random.randint(0, 255), green=random.randint(0, 255), blue=random.randint(0, 255))
+            bstick.set_color(red=NOTIFICATION_COLORS[alert_type][0], green=NOTIFICATION_COLORS[alert_type][1], blue=NOTIFICATION_COLORS[alert_type][2])
             sleep(5)
             bstick.turn_off()
 
+
 def startup_blinker():
     print("Starting Ori...")
-
-    # Check if there are any BlinkStick devices connected
     if len(blinkstick.find_all()) == 0:
         return
-
-    # Notify the alerts
+    
+    # Test the blinker
     for bstick in blinkstick.find_all():
         for i in range(5):
             bstick.set_color(red=random.randint(0, 255), green=random.randint(0, 255), blue=random.randint(0, 255))
